f91f24b2354ffd930660065e0114ffbe516ab694
Pedro Martin <pataruco@gmail.com>
2020-11-25 15:10:12 +0000

    Testing dynamic import inside functional component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+// @ts-nocheck
+import React, { useEffect, useState } from 'react';
 import { Deck, Slide, mdxComponentMap } from 'spectacle';
-import slides from '../slides/intro-to-html.mdx';
 import { MDXProvider } from '@mdx-js/react';
 
+import slides from '../slides/intro-to-html.mdx';
+
 const Home: React.FC = () => {
+  const [slidesRaw, setSlidesRaw] = useState(undefined);
+
+  const fetchSlides = async () => {
+    const { default: rawData } = await import('../slides/intro-to-html.mdx');
+    console.log({
+      slides,
+      rawData,
+    });
+    setSlidesRaw(rawData);
+  };
+
+  useEffect(() => {
+    fetchSlides();
+  }, []);
+
   return (
     <MDXProvider components={mdxComponentMap}>
       <Deck>
-        {slides
-          // @ts-ignore
-          .map((MDXSlide, i) => (
+        {Array.isArray(slidesRaw) &&
+          slidesRaw.map((MDXSlide, i) => (
             <Slide key={`slide-${i}`}>
               <MDXSlide />
             </Slide>
