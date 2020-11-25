371118a88d625a823883a306f3a6e195d9b87440
Pedro Martin <pataruco@gmail.com>
2020-11-25 13:09:35 +0000

    Create a slider boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import Routes from './routes';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Routes />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
---
+++ src/pages/home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Deck, Slide, Heading } from 'spectacle';
+
+const Home: React.FC = () => {
+  return (
+    <Deck>
+      <Slide>
+        <Heading>Welcome to Spectacle</Heading>
+      </Slide>
+    </Deck>
+  );
+};
+
+export default Home;
