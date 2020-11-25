54a173edb899ed44ead1548d3768f2fc70ba1ea3
Pedro Martin <pataruco@gmail.com>
2020-11-25 14:07:33 +0000

    before eject

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
-import { Deck, Slide, Heading } from 'spectacle';
+import React, { lazy, Suspense } from 'react';
+import { Deck, Slide, mdxComponentMap } from 'spectacle';
+import slides from '../slides/intro-to-html.mdx';
+
+// @ts-ignore
+// import { importMDX } from 'mdx.macro';
+import { MDXProvider } from '@mdx-js/react';
 
 const Home: React.FC = () => {
   return (
-    <Deck>
-      <Slide>
-        <Heading>Welcome to Spectacle</Heading>
-      </Slide>
-    </Deck>
+    <MDXProvider components={mdxComponentMap}>
+      <Deck>
+        {slides
+          // @ts-ignore
+          .map((MDXSlide, i) => [MDXSlide])
+          // @ts-ignore
+          .map(([MDXSlide], i) => (
+            // @ts-ignore
+            <Slide key={`slide-${i}`} slideNum={i}>
+              <MDXSlide />
+            </Slide>
+          ))}
+      </Deck>
+    </MDXProvider>
   );
 };
 
