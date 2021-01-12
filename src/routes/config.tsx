3f03279fc6d455217c1e2aaafc68f24c6427c2d0
Pedro Martin <pataruco@gmail.com>
2021-01-12 14:54:15 +0000

    add dynamic titles

--- src/routes/config.tsx
+++ src/routes/config.tsx
@@ -2,89 +2,188 @@ import React, { lazy, Suspense } from 'react';
 import SlidesDeck from '../components/slides-components/slides-deck';
 import { importMDX } from 'mdx.macro';
 import Week from '../pages/week';
+import Title from '../components/title';
 
 export const lessonRoutes = [
   {
     path: '/week-1/lesson-1',
-    component: () => <SlidesDeck slidesDeckName="01-html-basics" />,
+    component: () => (
+      <>
+        <Title week={1} lesson={1} />
+        <SlidesDeck slidesDeckName="01-html-basics" />)
+      </>
+    ),
   },
   {
     path: '/week-1/lesson-2',
     component: () => (
-      <SlidesDeck slidesDeckName="02-advanced-html-intro-to-css" />
+      <>
+        <Title week={1} lesson={2} />
+        <SlidesDeck slidesDeckName="02-advanced-html-intro-to-css" />
+      </>
     ),
   },
   {
     path: '/week-2/lesson-3',
-    component: () => <SlidesDeck slidesDeckName="03" />,
+    component: () => (
+      <>
+        <Title week={2} lesson={3} />
+        <SlidesDeck slidesDeckName="03" />
+      </>
+    ),
   },
   {
     path: '/week-2/lesson-4',
-    component: () => <SlidesDeck slidesDeckName="04" />,
+    component: () => (
+      <>
+        <Title week={2} lesson={4} />
+        <SlidesDeck slidesDeckName="04" />
+      </>
+    ),
   },
   {
     path: '/week-3/lesson-5',
-    component: () => <SlidesDeck slidesDeckName="05" />,
+    component: () => (
+      <>
+        <Title week={3} lesson={5} />
+        <SlidesDeck slidesDeckName="05" />
+      </>
+    ),
   },
   {
     path: '/week-3/lesson-6',
-    component: () => <SlidesDeck slidesDeckName="06" />,
+    component: () => (
+      <>
+        <Title week={3} lesson={6} />
+        <SlidesDeck slidesDeckName="06" />
+      </>
+    ),
   },
   {
     path: '/week-4/lesson-7',
-    component: () => <SlidesDeck slidesDeckName="07" />,
+    component: () => (
+      <>
+        <Title week={4} lesson={7} />
+        <SlidesDeck slidesDeckName="07" />
+      </>
+    ),
   },
   {
     path: '/week-4/lesson-8',
-    component: () => <SlidesDeck slidesDeckName="08" />,
+    component: () => (
+      <>
+        <Title week={4} lesson={8} />
+        <SlidesDeck slidesDeckName="08" />
+      </>
+    ),
   },
   {
     path: '/week-5/lesson-9',
-    component: () => <SlidesDeck slidesDeckName="09" />,
+    component: () => (
+      <>
+        <Title week={5} lesson={9} />
+        <SlidesDeck slidesDeckName="09" />
+      </>
+    ),
   },
   {
     path: '/week-5/lesson-10',
-    component: () => <SlidesDeck slidesDeckName="10" />,
+    component: () => (
+      <>
+        <Title week={5} lesson={10} />
+        <SlidesDeck slidesDeckName="10" />
+      </>
+    ),
   },
   {
     path: '/week-6/lesson-11',
-    component: () => <SlidesDeck slidesDeckName="11" />,
+    component: () => (
+      <>
+        <Title week={6} lesson={11} />
+        <SlidesDeck slidesDeckName="11" />
+      </>
+    ),
   },
   {
     path: '/week-6/lesson-12',
-    component: () => <SlidesDeck slidesDeckName="12" />,
+    component: () => (
+      <>
+        <Title week={6} lesson={12} />
+        <SlidesDeck slidesDeckName="12" />
+      </>
+    ),
   },
   {
     path: '/week-7/lesson-13',
-    component: () => <SlidesDeck slidesDeckName="13" />,
+    component: () => (
+      <>
+        <Title week={7} lesson={13} />
+        <SlidesDeck slidesDeckName="13" />
+      </>
+    ),
   },
   {
     path: '/week-7/lesson-14',
-    component: () => <SlidesDeck slidesDeckName="14" />,
+    component: () => (
+      <>
+        <Title week={7} lesson={14} />
+        <SlidesDeck slidesDeckName="14" />
+      </>
+    ),
   },
   {
     path: '/week-8/lesson-15',
-    component: () => <SlidesDeck slidesDeckName="15" />,
+    component: () => (
+      <>
+        <Title week={8} lesson={15} />
+        <SlidesDeck slidesDeckName="15" />
+      </>
+    ),
   },
   {
     path: '/week-8/lesson-16',
-    component: () => <SlidesDeck slidesDeckName="16" />,
+    component: () => (
+      <>
+        <Title week={8} lesson={16} />
+        <SlidesDeck slidesDeckName="16" />
+      </>
+    ),
   },
   {
     path: '/week-9/lesson-17',
-    component: () => <SlidesDeck slidesDeckName="17" />,
+    component: () => (
+      <>
+        <Title week={9} lesson={17} />
+        <SlidesDeck slidesDeckName="17" />
+      </>
+    ),
   },
   {
     path: '/week-9/lesson-18',
-    component: () => <SlidesDeck slidesDeckName="18" />,
+    component: () => (
+      <>
+        <Title week={9} lesson={18} />
+        <SlidesDeck slidesDeckName="18" />
+      </>
+    ),
   },
   {
     path: '/week-10/lesson-19',
-    component: () => <SlidesDeck slidesDeckName="19" />,
+    component: () => (
+      <>
+        <Title week={10} lesson={19} />
+        <SlidesDeck slidesDeckName="19" />
+      </>
+    ),
   },
   {
     path: '/week-10/lesson-20',
-    component: () => <SlidesDeck slidesDeckName="20" />,
+    component: () => (
+      <>
+        <Title week={10} lesson={20} />
+        <SlidesDeck slidesDeckName="20" />
+      </>
+    ),
   },
 ];
 
@@ -95,6 +194,7 @@ export const weekRoutes = [
       const Content = lazy(() => importMDX('../weeks/1.mdx'));
       return (
         <Week>
+          <Title week={1} />
           <Suspense fallback={<div>Loading...</div>}>
             <Content />
           </Suspense>
@@ -108,6 +208,7 @@ export const weekRoutes = [
       const Content = lazy(() => importMDX('../weeks/2.mdx'));
       return (
         <Week>
+          <Title week={2} />
           <Suspense fallback={<div>Loading...</div>}>
             <Content />
           </Suspense>
@@ -121,6 +222,7 @@ export const weekRoutes = [
       const Content = lazy(() => importMDX('../weeks/3.mdx'));
       return (
         <Week>
+          <Title week={3} />
           <Suspense fallback={<div>Loading...</div>}>
             <Content />
           </Suspense>
@@ -134,6 +236,7 @@ export const weekRoutes = [
       const Content = lazy(() => importMDX('../weeks/4.mdx'));
       return (
         <Week>
+          <Title week={4} />
           <Suspense fallback={<div>Loading...</div>}>
             <Content />
           </Suspense>
@@ -147,6 +250,7 @@ export const weekRoutes = [
       const Content = lazy(() => importMDX('../weeks/5.mdx'));
       return (
         <Week>
+          <Title week={5} />
           <Suspense fallback={<div>Loading...</div>}>
             <Content />
           </Suspense>
@@ -160,6 +264,7 @@ export const weekRoutes = [
       const Content = lazy(() => importMDX('../weeks/6.mdx'));
       return (
         <Week>
+          <Title week={6} />
           <Suspense fallback={<div>Loading...</div>}>
             <Content />
           </Suspense>
@@ -173,6 +278,7 @@ export const weekRoutes = [
       const Content = lazy(() => importMDX('../weeks/7.mdx'));
       return (
         <Week>
+          <Title week={7} />
           <Suspense fallback={<div>Loading...</div>}>
             <Content />
           </Suspense>
@@ -186,6 +292,7 @@ export const weekRoutes = [
       const Content = lazy(() => importMDX('../weeks/8.mdx'));
       return (
         <Week>
+          <Title week={8} />
           <Suspense fallback={<div>Loading...</div>}>
             <Content />
           </Suspense>
@@ -199,6 +306,7 @@ export const weekRoutes = [
       const Content = lazy(() => importMDX('../weeks/9.mdx'));
       return (
         <Week>
+          <Title week={9} />
           <Suspense fallback={<div>Loading...</div>}>
             <Content />
           </Suspense>
@@ -212,6 +320,7 @@ export const weekRoutes = [
       const Content = lazy(() => importMDX('../weeks/10.mdx'));
       return (
         <Week>
+          <Title week={10} />
           <Suspense fallback={<div>Loading...</div>}>
             <Content />
           </Suspense>
