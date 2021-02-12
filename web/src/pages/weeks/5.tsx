4ad0ce56d023cc7e0d2926e1bdaa88f87d6fba4c
Pedro Martin <pataruco@gmail.com>
2021-02-12 12:55:37 +0000

    add bonuses  and resources

--- web/src/pages/weeks/5.tsx
+++ web/src/pages/weeks/5.tsx
@@ -1,12 +1,50 @@
 import React from 'react';
 import Week from '../../components/week';
 
-const Week5 = () => (
-  <Week
-    weekNumber={5}
-    firstLessonTitle="Intro to JavaScript"
-    secondLessonTitle="Document Object Model (DOM)"
-  />
-);
+const resources = [
+  [
+    'MDN: JavaScript Introduction',
+    'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Introduction',
+  ],
+  [
+    'MDN: Learning JavaScript',
+    'https://developer.mozilla.org/en-US/Learn/JavaScript',
+  ],
+  [
+    'Stack Overflow: When should I use a semicolon after curly braces?',
+    'http://stackoverflow.com/questions/2717949/when-should-i-use-a-semicolon-after-curly-braces',
+  ],
+  [
+    'MDN: Document',
+    'https://developer.mozilla.org/en-US/docs/Web/API/Document',
+  ],
+  ['MDN: Window', 'https://developer.mozilla.org/en-US/docs/Web/API/Window'],
+  ['MDN: Web APIs', 'https://developer.mozilla.org/en-US/docs/Web/API'],
+  [
+    'MDN: Event object',
+    'https://developer.mozilla.org/en-US/docs/Web/API/Event',
+  ],
+];
+
+const bonuses = [
+  [
+    'Read through MDN JavaScript Bbasics',
+    'https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics',
+  ],
+  [
+    "Start Code Academy's Introduction to JavaScript",
+    'https://www.codecademy.com/learn/introduction-to-javascript',
+  ],
+];
+
+const weekConfig = {
+  firstLessonTitle: 'Intro to JavaScript',
+  bonuses,
+  resources,
+  secondLessonTitle: 'Document Object Model (DOM)',
+  weekNumber: 5,
+};
+
+const Week5 = () => <Week {...weekConfig} />;
 
 export default Week5;
