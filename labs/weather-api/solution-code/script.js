047994f2333750bcdfea500d1d76a0fb1699d0f1
Pedro Martin <pataruco@gmail.com>
2021-02-24 14:45:12 +0000

    Set weather api solution

--- labs/weather-api/solution-code/script.js
+++ labs/weather-api/solution-code/script.js
@@ -1,9 +1,22 @@
+// Elements
+
+const form = document.querySelector('#weather-search');
+const tempElement = document.querySelector('#temp');
+const descriptionElement = document.querySelector('#description');
+const cityElement = document.getElementById('city');
+
+// Event
+
+form.addEventListener('submit', handleWeatherRequest);
+
+// Execution
+
 async function handleWeatherRequest(event) {
   // stop the form submit from refreshing the page
   event.preventDefault();
-
-  const city = document.getElementById('city').value;
+  const city = cityElement.value;
   const apiKey = '870b1b10b58578f725b70b13aff6c357';
+
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`;
   console.log(url);
 
@@ -13,7 +26,7 @@ async function handleWeatherRequest(event) {
   // convert the response to json data
   // save the json in a weatherData variable
   const weatherData = await response.json();
-  console.log(weatherData);
+  console.log({ weatherData });
 
   // get the weather descripton from response data
   const weatherDescription = weatherData.weather[0].description;
@@ -23,14 +36,10 @@ async function handleWeatherRequest(event) {
 
   // update the temperature and decription on the page
   document.querySelector('#temp').innerHTML = `
-		${tempCelcius}<sup>o</sup>C
-	`;
+  		${tempCelcius}<sup>o</sup>C
+  	`;
   document.querySelector('#description').innerHTML = `
-		The weather in <strong>${city}</strong> is <br>
-		<strong>${weatherDescription}</strong>
-	`;
+  		The weather in <strong>${city}</strong> is <br>
+  		<strong>${weatherDescription}</strong>
+  	`;
 }
-
-document
-  .querySelector('#weather-search')
-  .addEventListener('submit', handleWeatherRequest);
