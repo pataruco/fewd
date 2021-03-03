6b30eb69054d68aae666c898aba89d97e974306d
Pedro Martin <pataruco@gmail.com>
2021-03-03 21:40:43 +0000

    draft slide navigation injection

--- web/src/components/slides-deck.tsx
+++ web/src/components/slides-deck.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import '../styles/slide-index.scss';
-
+import { useHistory } from 'react-router-dom';
 const { PUBLIC_URL } = process.env;
 
 interface SlidesDeckProps {
@@ -12,23 +12,36 @@ interface GetSlideIndex {
 }
 
 interface Slide {
-  // Function returning the slides number (0..N-1, where N is the number of slides)
   getSlideIndex: GetSlideIndex;
-
-  // The notes for the slide
   notes: string;
-
-  // The slide properties extracted from the Markdown
   properties: {
     class: string;
     name: string;
   };
-
-  // The Markdown representing the slide
   content: string;
 }
 
+const slideNavigationClass = 'slide-navigation';
+
+const createAnchor = (history: History['state']) => {
+  const {
+    location: { pathname },
+  } = history;
+
+  const pageToNavigate = `/${pathname.split('/')[1]}`;
+  const navigationText = pageToNavigate.split('-').pop();
+
+  const anchor = document.createElement('a');
+  anchor.classList.add(slideNavigationClass);
+  anchor.setAttribute('href', pageToNavigate);
+  anchor.innerText = `back to week ${navigationText}`;
+
+  return anchor;
+};
+
 const SlidesDeck: React.FC<SlidesDeckProps> = ({ slidesDeckName }) => {
+  const history = useHistory();
+
   useEffect(() => {
     // @ts-ignore
     const slides = remark.create({
@@ -48,9 +61,20 @@ const SlidesDeck: React.FC<SlidesDeckProps> = ({ slidesDeckName }) => {
     });
 
     slides.on('showSlide', (slide: Slide) => {
-      console.log('hola');
+      const slideIndex = slide.getSlideIndex();
+
+      const slideElements = document.querySelectorAll('.remark-slide-content');
+      const anchors = document.querySelectorAll(`.${slideNavigationClass}`);
+      anchors.forEach((anchor) => anchor.remove());
+
+      console.log({ anchors });
+
+      if (slideElements) {
+        const anchor = createAnchor(history);
+        slideElements[slideIndex].appendChild(anchor);
+      }
     });
-  }, [slidesDeckName]);
+  }, [slidesDeckName, history]);
 
   return null;
 };
